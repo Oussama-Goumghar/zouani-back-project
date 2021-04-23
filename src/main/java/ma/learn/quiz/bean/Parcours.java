8991864f42dc9e7e6029e0d92e22d9917c6f9c00
meryamlouhaidy <79732423+meryamlouhaidy@users.noreply.github.com>
2021-04-23 14:16:15 +0200

    cc

--- src/main/java/ma/learn/quiz/bean/Parcours.java
+++ src/main/java/ma/learn/quiz/bean/Parcours.java
@@ -1,69 +1,55 @@
 package ma.learn.quiz.bean;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import javax.persistence.*;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 @Entity
 public class Parcours {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String code;
-    private String libelle;
-    private double duree;
-   
-
-	
-
-	public Parcours(String code, String libelle, double duree) {
-		super();
-		this.code = code;
-		this.libelle = libelle;
-		this.duree = duree;
+	  @Id
+	    @GeneratedValue(strategy = GenerationType.AUTO)
+	    private Long id;
+	    private String code;
+	    private String libelle;
+	    private String ref;
+	    @OneToOne
+	    private Centre centre;
+	  
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getCode() {
+			return code;
+		}
+		public void setCode(String code) {
+			this.code = code;
+		}
+		public String getLibelle() {
+			return libelle;
+		}
+		public void setLibelle(String libelle) {
+			this.libelle = libelle;
+		}
+		public Centre getCentre() {
+			return centre;
+		}
+		public void setCentre(Centre centre) {
+			this.centre = centre;
+		}
+		public String getRef() {
+			return ref;
+		}
+		public void setRef(String ref) {
+			this.ref = ref;
+		}
 		
-	}
-
-	public Parcours() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getLibelle() {
-        return libelle;
-    }
-
-    public void setLibelle(String libelle) {
-        this.libelle = libelle;
-    }
-
-
-
-    public double getDuree() {
-        return duree;
-    }
-
-    public void setDuree(double duree) {
-        this.duree = duree;
-    }
-
-
 }
+	
