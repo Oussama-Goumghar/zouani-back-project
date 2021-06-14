fc452065c7c9d496c6d9c707087f70324ee2ef8e
mohcinemourchid99 <mohcine.mourchid@edu.uca.ma>
2021-06-14 18:26:22 +0100

    quiz

--- src/main/java/ma/learn/quiz/rest/ReponseRest.java
+++ src/main/java/ma/learn/quiz/rest/ReponseRest.java
@@ -9,13 +9,14 @@ import ma.learn.quiz.service.ReponseService;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "learn/reponse")
 public class ReponseRest {
-    @GetMapping("/question/ref/{ref}")
-    public List<Reponse> findByQuestionRef(@PathVariable String ref) {
-        return reponseService.findByQuestionRef(ref);
+    @GetMapping("/question/id/{id}")
+    public List<Reponse> findByQuestionId(@PathVariable Long id) {
+        return reponseService.findByQuestionId(id);
     }
     @GetMapping("/ref/{ref}")
     public Reponse findByRef(@PathVariable String ref) {
@@ -46,8 +47,11 @@ public class ReponseRest {
     @Autowired
     private ReponseService reponseService;
 
-    
-    @GetMapping("/etatReponse/{etatReponse}")
+    @GetMapping("/id/{id}")
+    public Optional<Reponse> findById(@PathVariable Long id) {
+		return reponseService.findById(id);
+	}
+	@GetMapping("/etatReponse/{etatReponse}")
 	public List<Reponse> findByEtatReponse(@PathVariable String etatReponse) {
 		return reponseService.findByEtatReponse(etatReponse);
 	}
@@ -57,9 +61,9 @@ public class ReponseRest {
 		return reponseService.findByQuestionNumero(numero);
 	}
 	
-	@GetMapping("/criteria/ref/{ref}")
-	public List<Reponse> findByCriterial(@PathVariable String ref) {
-		return reponseService.findByCriterial(ref);
+	@GetMapping("/criteria/id/{id}")
+	public List<Reponse> findByCriterial(@PathVariable Long id) {
+		return reponseService.findByCriterial(id);
 	}
 
 }
